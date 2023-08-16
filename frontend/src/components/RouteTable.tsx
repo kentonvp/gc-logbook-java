16902c627b82cc43302f2060944d4c1ae94213d0
Kenton Van Peursem <kentonvanp@gmail.com>
2023-08-15 16:40:43 -1000

    Add clear filter button

--- frontend/src/components/RouteTable.tsx
+++ frontend/src/components/RouteTable.tsx
@@ -3,10 +3,11 @@ import "./RouteTable.css"
 import PatientCard from "./PatientCard";
 import { Patient } from "~/patient";
 
-const fetchPatients = async (limit: number, offset: number, specialty: string, testName: string, indication: string) => {
+const fetchPatients = async (limit: number, offset: number, specialty: string, indication: string, summary: string, testName: string) => {
     const params = new URLSearchParams({
         testName: testName,
-        indication: indication
+        indication: indication,
+        summary: summary,
     });
     if (specialty !== "All") {
         params.append("specialty", specialty);
@@ -24,53 +25,69 @@ export default function RouteTable() {
     const [specialty, setSpecialty] = createSignal("All");
 
     const [patients] = createResource(
-        () => [limit(), offset(), specialty(), testNameFilter(), indicationFilter()] as const,
-        ([limit, offset, specialty, testName, indication]) => fetchPatients(limit, offset, specialty, testName, indication)
+        () => [limit(), offset(), specialty(), indicationFilter(), summaryFilter(), testNameFilter()] as const,
+        ([limit, offset, specialty, indication, summary, testName]) => fetchPatients(limit, offset, specialty, indication, summary, testName)
     );
 
     const newPatient = () => {
         console.log("Go to new patient view")
-    }
+    };
 
-    const showMore = () => {
+    const loadMore = () => {
         setOffset(offset() + limit());
         console.log("Offset=" + offset() + " Limit=" + limit());
-    }
+    };
+
+    let indicationFilterRef: HTMLInputElement;
+    let summaryFilterRef: HTMLInputElement;
+    let testNameFilterRef: HTMLInputElement;
+
+    const clearInputFilters = () => {
+        setSpecialty("All");
+        setIndicationFilter("");
+        setSummaryFilter("");
+        setTestNameFilter("");
+
+        indicationFilterRef.value = "";
+        summaryFilterRef.value = "";
+        testNameFilterRef.value = "";
+    };
 
     return (
         <>
-        <h1 class="text-center my-2">Patients Info</h1>
-        <div class="flex flex-row gap-2">
-            <button class='bg-green-500 hover:bg-green-600 text-white py-2 px-4 ml-2 rounded-lg active:bg-green-700' onClick={newPatient}>Create New Patient</button>
-            <div class="grow">{/* whitespace */}</div>
-            <select value={specialty()} onChange={(e) => setSpecialty(e.target.value)}>
-                <option value="All">All</option>
-                <option value="GENERAL">General</option>
-                <option value="PEDIATRIC">Pediatric</option>
-                <option value="LAB">Lab</option>
-                <option value="CANCER">Cancer</option>
-                <option value="PRENATAL">Prenatal</option>
-                <option value="OTHER">Other</option>
-            </select>
-            <input class="px-4" type="string" placeholder="Test name filter" onChange={(e) => setTestNameFilter(e.target.value)}/>
-            <input class="px-4" type="string" placeholder="Indication filter" onChange={(e) => setIndicationFilter(e.target.value)}/>
-            <input class="px-4 mr-2" type="string" placeholder="Summary filter" onChange={(e) => setSummaryFilter(e.target.value)}/>
-        </div>
+            <h1 class="text-center my-2">Patients Info</h1>
+            <div class="flex flex-row gap-2">
+                <button class='bg-green-500 hover:bg-green-600 text-white py-2 px-4 ml-2 rounded-lg active:bg-green-700' onClick={newPatient}>Create New Patient</button>
+                <div class="grow">{/* whitespace */}</div>
+                <select value={specialty()} onChange={(e) => setSpecialty(e.target.value)}>
+                    <option value="All">All</option>
+                    <option value="GENERAL">General</option>
+                    <option value="PEDIATRIC">Pediatric</option>
+                    <option value="LAB">Lab</option>
+                    <option value="CANCER">Cancer</option>
+                    <option value="PRENATAL">Prenatal</option>
+                    <option value="OTHER">Other</option>
+                </select>
+                <input ref={indicationFilterRef} class="filter-box" type="string" placeholder="Indication filter" onChange={(e) => setIndicationFilter(e.target.value)} />
+                <input ref={summaryFilterRef} class="filter-box" type="string" placeholder="Summary filter" onChange={(e) => setSummaryFilter(e.target.value)} />
+                <input ref={testNameFilterRef} class="filter-box" type="string" placeholder="Test name filter" onChange={(e) => setTestNameFilter(e.target.value)} />
+                <button class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-3 mr-2 rounded-lg active:bg-blue-700" onClick={clearInputFilters}>Clear Filters</button>
+            </div>
 
-        <Show when={patients()} fallback={<h2 class="p-4 text-center">Loading...</h2>}>
-            <div>
-                <For each={patients()} fallback={<h2 class="p-4 text-center">Oops, no patients with given filters...</h2>}>
-                    {(patient: Patient) => (
-                        <PatientCard patient={patient} />
-                    )}
-                </For>
-            </div>
-            <div class="flex flex-row">
-                <div class="grow">{/* whitespace */}</div>
-                <button class='bg-gray-400 hover:bg-gray-500 text-white py-2 px-4 rounded-lg active:bg-gray-600 w-1/4' onClick={showMore}>Load More</button>
-                <div class="grow">{/* whitespace */}</div>
-            </div>
-        </Show>
+            <Show when={patients()} fallback={<h2 class="p-4 text-center">Loading...</h2>}>
+                <div>
+                    <For each={patients()} fallback={<h2 class="p-4 text-center">Oops, no patients with given filters...</h2>}>
+                        {(patient: Patient) => (
+                            <PatientCard patient={patient} />
+                        )}
+                    </For>
+                </div>
+                <div class="flex flex-row">
+                    <div class="grow">{/* whitespace */}</div>
+                    <button class='bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded-lg active:bg-gray-700 w-1/4' onClick={loadMore}>Load More</button>
+                    <div class="grow">{/* whitespace */}</div>
+                </div>
+            </Show>
         </>
     );
 }
