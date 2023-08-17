50378c8bf3de06ad57be45ee4461218fc4835533
Kenton Van Peursem <kentonvanp@gmail.com>
2023-08-16 16:55:31 -1000

    Positions new patient window at center of screen

--- frontend/src/components/RouteTable.tsx
+++ frontend/src/components/RouteTable.tsx
@@ -62,48 +62,46 @@ export default function RouteTable() {
     };
 
     return (
-        <>
-        <div>
-            <div class={onNewPatientView() ? "opacity-25" : ""}>
-            <h1 class="text-center my-2">Patients Info</h1>
-            <div class="flex flex-row gap-2">
-                <button class='bg-green-500 hover:bg-green-600 text-white py-2 px-4 ml-2 rounded-lg active:bg-green-700' onClick={toggleNewPatientView}>Create New Patient</button>
-                <div class="grow">{/* whitespace */}</div>
-                <select value={specialty()} onChange={(e) => setSpecialty(e.target.value)}>
-                    <option value="All">All</option>
-                    <option value="GENERAL">General</option>
-                    <option value="PEDIATRIC">Pediatric</option>
-                    <option value="LAB">Lab</option>
-                    <option value="CANCER">Cancer</option>
-                    <option value="PRENATAL">Prenatal</option>
-                    <option value="OTHER">Other</option>
-                </select>
-                <input ref={indicationFilterRef} class="filter-box" type="string" placeholder="Indication filter" onChange={(e) => setIndicationFilter(e.target.value)} />
-                <input ref={summaryFilterRef} class="filter-box" type="string" placeholder="Summary filter" onChange={(e) => setSummaryFilter(e.target.value)} />
-                <input ref={testNameFilterRef} class="filter-box" type="string" placeholder="Test name filter" onChange={(e) => setTestNameFilter(e.target.value)} />
-                <button class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-3 mr-2 rounded-lg active:bg-blue-700" onClick={clearInputFilters}>Clear Filters</button>
-            </div>
+        <div class="flex flex-col">
+            <div class="static">
+                <div class={onNewPatientView() ? "opacity-50" : ""}>
+                    <h1 class="text-center my-2">Patients Info</h1>
+                    <div class="flex flex-row gap-2">
+                        <button class='btn-purple ml-2' onClick={() => toggleNewPatientView(true)}>Create New Patient</button>
+                        <div class="grow">{/* whitespace */}</div>
+                        <select value={specialty()} onChange={(e) => setSpecialty(e.target.value)}>
+                            <option value="All">All</option>
+                            <option value="GENERAL">General</option>
+                            <option value="PEDIATRIC">Pediatric</option>
+                            <option value="LAB">Lab</option>
+                            <option value="CANCER">Cancer</option>
+                            <option value="PRENATAL">Prenatal</option>
+                            <option value="OTHER">Other</option>
+                        </select>
+                        <input ref={indicationFilterRef} class="filter-box" type="string" placeholder="Indication filter" onChange={(e) => setIndicationFilter(e.target.value)} />
+                        <input ref={summaryFilterRef} class="filter-box" type="string" placeholder="Summary filter" onChange={(e) => setSummaryFilter(e.target.value)} />
+                        <input ref={testNameFilterRef} class="filter-box" type="string" placeholder="Test name filter" onChange={(e) => setTestNameFilter(e.target.value)} />
+                        <button class="btn-purple mr-2" onClick={clearInputFilters}>Clear Filters</button>
+                    </div>
 
-            <Show when={patients()} fallback={<h2 class="p-4 text-center">Loading...</h2>}>
-                <div>
-                    <For each={patients()} fallback={<h2 class="p-4 text-center">Oops, no patients with given filters...</h2>}>
-                        {(patient: Patient) => (
-                            <PatientCard patient={patient} delete_callback={() => setNewPatient(true)} />
-                        )}
-                    </For>
+                    <Show when={patients()} fallback={<h2 class="p-4 text-center">Loading...</h2>}>
+                        <div>
+                            <For each={patients()} fallback={<h2 class="p-4 text-center">Oops, no patients with given filters...</h2>}>
+                                {(patient: Patient) => (
+                                    <PatientCard patient={patient} delete_callback={() => setNewPatient(true)} />
+                                )}
+                            </For>
+                        </div>
+                    </Show>
                 </div>
-                <div class="flex flex-row">
-                    <div class="grow">{/* whitespace */}</div>
-                    <button class='bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded-lg active:bg-gray-700 w-1/4' onClick={loadMore}>Load More</button>
-                    <div class="grow">{/* whitespace */}</div>
-                </div>
-            </Show>
             </div>
-
-            <div class={"absolute inset-1/4 z-40 bg-gray-300 rounded-lg shadow " + (!onNewPatientView() ? "hidden" : "")}>
-                <NewPatientForm success_callback={succesfulNewPatient} cancel_callback={() => toggleNewPatientView(false)}/>
+            <div class="grow">{/* whitespace */}</div>
+            <div class={"absolute inset-1/4" + (!onNewPatientView() ? " hidden" : "")}>
+                <NewPatientForm success_callback={succesfulNewPatient} cancel_callback={() => toggleNewPatientView(false)} />
+            </div>
+            <div class="flex justify-center">
+                <button class='btn-purple w-1/4' onClick={loadMore}>Load More</button>
             </div>
         </div>
-        </>
     );
 }
